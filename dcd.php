70cac957c20836793134cc2fbe88518f217be7cd
Stephen Wu <wujstep@gmail.com>
2016-05-09 02:20:34 -0400

    add-event done
    template reorganized, minor fixes and cleanup as well

--- dcd.php
+++ dcd.php
@@ -7,15 +7,28 @@
 <link href="resources/css/dcd.css" rel="stylesheet">
 </head>
 <body>
-	<h1>DCD Generator</h1><br>
+	<h1>DCD Generator</h1>
 	Date: <input type="text" id="dcd-start"> to 
-	<input type="text" id="dcd-end"><br>
-	<div style="display:none">Template: <select id="template"><option id="stemee" selected>STEM EE</option></select></div><br><br>
+	<input type="text" id="dcd-end"><br><br>
 	<input type="button" id="submit" value="Submit">&nbsp;<input type="button" id="copy" value="Copy"/><br><br>
 	<div id="data" class="fluid" style="display:none">
 		<iframe id="output">
 		</iframe>
 	</div>
+	
+	<div id="event-add-dialog" title="Add event" style="display:none;">
+	  <form id="event-add-form">
+		<fieldset id="event-add-fieldset">
+		  <label for="name">Name</label><br>
+		  <input type="text" name="name" id="event-add-name" value="" class="ui-widget-content" autocomplete="off"><br>
+		  <label for="info">Info (Optional)</label><br>
+		  <input type="text" name="info" id="event-add-info" placeholder="e.g. Tuesday, April 12, 5:20PM-6:00PM, Evans Lab Room 2004" value="" class="ui-widget-content" autocomplete="off"><br>
+		  <label for="desc">Description</label><br>
+		  <textarea name="desc" id="event-add-notes" value="" class="ui-widget-content" autocomplete="off"></textarea>
+		</fieldset>
+	  </form>
+	</div>	
+
 	<script src="lib/jquery/jquery.min.js"></script>
 	<script src="lib/jquery/jquery-ui.min.js"></script>
 	<script src="lib/date.js"></script>
--- resources/js/dcd.js
+++ resources/js/dcd.js
@@ -1,4 +1,4 @@
-// Creates a CORS request in a cross-browser manner
+// Creates a CORS request in a cross-browser manner (from teamup API website)
 function createCORSRequest(method, url) {
     var apiKey = '5668a0a6945a3199ababf3e17fe4cea817fb6d44520379d6a9515b5eb24d74ce';
     var xhr = new XMLHttpRequest();
@@ -30,7 +30,7 @@ function createCORSRequest(method, url) {
     return xhr;
 }
 
-// Sends the actual CORS request.
+// Sends the actual CORS request, (from teamup API website)
 function makeCorsRequest(url, successCallback, errorCallback) {
     var xhr = createCORSRequest('GET', url);
     if (!xhr) {
@@ -63,9 +63,9 @@ function generateDCD(data, body) {
 			$.each(data, function (item, value) {
 				if (item == "events") {
 					$.each(value, function (i, object) {
-						var calendar = "c" + object["subcalendar_id"];
+						var calendar = "cal-" + object["subcalendar_id"];
 						if ($(self).hasClass(calendar)) {
-							$(self).append("<li class='toc-event e" + object["id"] +"'>" + object["title"] + "</li>")
+							$(self).append("<li class='toc-event event-" + object["id"] +"'>" + object["title"] + "</li>")
 						}
 					}); 
 				}
@@ -75,7 +75,7 @@ function generateDCD(data, body) {
 			$.each(data, function (item, value) {
 				if (item == "events") {
 					$.each(value, function (i, object) {
-						var calendar = "c" + object["subcalendar_id"];
+						var calendar = "cal-" + object["subcalendar_id"];
 						if ($(self).hasClass(calendar)) {
 							var ev = "";
 							var cal = 0;
@@ -88,7 +88,7 @@ function generateDCD(data, body) {
 								} else if (subI == "id") {
 									id = subObject;
 								} else if (subI == "title") {
-									ev = "<span class='c"+cal+" info-event-title'>" + subObject + '</span><br>' + ev;
+									ev = "<span class='cal-"+cal+" info-event-title'>" + subObject + '</span><br>' + ev;
 								} else if (subI == "start_dt") {
 									startDate = subObject.substring(0,19);	
 								} else if (subI == "end_dt") {
@@ -114,67 +114,43 @@ function generateDCD(data, body) {
 									ev = "<span class='info-event-desc'>" + ev + "</span><br><span class='info-event-notes'>" + subObject.replace(/(<p[^>]+?>|<p>|<\/p>)/img, "").replace(/\n/g, "<br>") + "</span>";
 								}
 							});
-							$(self).append("<span class='info-event e"+id+"'>"+ev+"</span><br><br>");
+							$(self).append("<br><span class='info-event event-"+id+"'>"+ev+"</span><br>");
 						}
 					});
 				}
 			});		
-		} else if ($(this).hasClass("output")) {
-			$(this).html(generateDCD(data));
 		}
 	});
 	return results;
 }
 
-function generateDCD2(data) { // Dump ALL the events! -- Old version
-	var results = '';
-	$.each(data, function (item, value) {
-		if (item == "events") {
-			$.each(value, function (i, object) {
-				var ev = "";
-				var cal = 0;
-				var startDate = "";
-				var endDate = "";
-				var id = "";
-				$.each(object, function (subI, subObject) {
-					if (subI == "subcalendar_id") {
-						cal = subObject;
-					} else if (subI == "id") {
-						id = subObject;
-					} else if (subI == "title") {
-						// temporary test
-						ev = "<span class='c"+cal+"'>" + subObject + '</span><br>' + ev;
-					} else if (subI == "start_dt") {
-						startDate = subObject.substring(0,19);	
-					} else if (subI == "end_dt") {
-						endDate = subObject.substring(0,19);
-					} else if (subI == "all_day") {
-						//alert(subObject + " " + startDate);
-						if (startDate.length > 0) {
-							var allday = (subObject) ? "" : ", h:sstt";
-							var date = Date.parse(startDate).toString("dddd, MMMM d" + allday);
-							//alert(startDate.substring(0, 10));
-							if (startDate.substring(0, 10) == endDate.substring(0, 10)) { // if end date (mm/dd/yy) is same
-								if (!subObject) { // If not all day
-									date = date + "-" + (Date.parse(endDate).toString("h:sstt")) || "<b>Invalid date</b>"; // Add end-time
-								}
-							} else { // Otherwise
-								date = date + "-" + (Date.parse(endDate).toString("dddd, MMMM d" + allday)) || "<b>Invalid date</b>"; // Add end date/time
-							}
-							ev = ev + date;
-						}
-					} else if (subI == "location" && subObject.length > 0) {
-						ev = ev + ", " + subObject;
-					} else if (subI == "notes") {
-						ev = ev + "<br>" + subObject.replace(/(<p[^>]+?>|<p>|<\/p>)/img, "").replace(/\n/g, "<br>");
-					}
-					console.log(subI + "=" + subObject);
-				});
-				results = results + ev + "<br><br>";
-			});
-		}
+function numberEvents() {
+	var count = 1;
+	$("iframe#output").contents().find("ul.count, div.count").each(function() {
+		if ($(this).hasClass("count-reset")) count = 1;
+		$(this).find("li.toc-event, span.info-event-title").each(function() {
+			var i = 0;
+			$(this).find("span.counter").remove();
+			$(this).prepend("<span class='counter'>"+ count + ".&nbsp;</span>"); // first time numbering
+			count++;
+		});
 	});
-	return results;
+}
+
+function getNewEventId() {
+	var id = 1;
+	while ($("iframe#output").contents().find("li.event-"+id+", span.event-"+id).length > 0) {
+		id++;
+	}
+	return id;
+}
+
+function addEvent(set, eventId, eventName, eventInfo, eventNotes) {
+	$("iframe#output").contents().find("ul." + set).first().append("<li class='toc-event event-"+eventId+" ui-sortable-handle'>"+eventName+"</li>");
+	var info = (eventInfo.length > 0) ? "<br>" + eventInfo : "";
+	var notes = (eventNotes.length > 0) ? "<br><span class='info-event-notes'>"+eventNotes+"</span>" : "";
+	$("iframe#output").contents().find("div." + set).first().append("<br><span class='info-event event-"+eventId+"'><span class='info-event-desc'><span class='info-event-title "+set+"'>"+eventName+"</span>"+info+'</span>'+notes+"</span><br>");
+	numberEvents();
 }
 
 $(function() {
@@ -201,6 +177,7 @@ $(function() {
 	var sundayAfter = Date.parse("2016-04-17").toString("yyyy-MM-dd");
 	//var nextMonday =  Date.today().next().monday().toString("yyyy-MM-dd");
 	//var sundayAfter = Date.today().next().monday().add(6).day().toString("yyyy-MM-dd");
+	
 	$("#dcd-start").attr('value', nextMonday);
 	$("#dcd-end").attr('value', sundayAfter);
 
@@ -211,18 +188,23 @@ $(function() {
 		$("#copy").button("enable");
 		var startDate = $("#dcd-start").prop('value');
 		var endDate = $("#dcd-end").prop('value');
-		var template = $("#template option:selected").attr('id');
 		makeCorsRequest(
 			'https://api.teamup.com/kse89a84dcb543ed5e/events?startDate='+startDate+'&endDate='+endDate,
-			function(xhr) {
+			function(xhr) { // Success
 				var data = JSON.parse(xhr.responseText);
 				$('#data').show();
-				$('iframe#output').attr('src', 'resources/templates/' +template+'.html');
+				$('iframe#output').attr('src', 'resources/templates/stemee.html');
 				$('iframe#output').load(function() {
-					generateDCD(data, $("iframe#output").contents().find("#content"), template);
+					var iframe = $('iframe#output').contents();
+					
+					generateDCD(data, iframe.find("#content")); // Generate DCD
+					
+					// Add jQueryUI, Event CSS (for icons and sortables)
 					var jQueryUICSS = "<link rel='stylesheet' href='../../lib/jquery/jquery-ui.min.css'>";
 					var eventsCSS = "<link rel='stylesheet' href='events.css'>";
-					$("iframe#output").contents().find("head").append(jQueryUICSS + eventsCSS);
+					iframe.find("head").append(jQueryUICSS + eventsCSS);
+					
+					// Make TOC events sortable
 					$("iframe#output").contents().find("ul.toc").sortable({ 
 						connectWith: ".toc", 
 						placeholder: "ui-state-highlight", 
@@ -233,6 +215,7 @@ $(function() {
 					}).disableSelection();
 					numberEvents();
 					
+					// Add sort icon on hover
 					var $sortIcon = $("<span class='event-drag ui-icon ui-icon-arrowthick-2-n-s'></span>");
 					$("iframe#output").contents().find(".toc-event").hover(function() {
 						$(this).prepend($sortIcon);
@@ -240,19 +223,23 @@ $(function() {
 						$sortIcon.remove();
 					});
 					
+					// Add add event option on hover
 					var $addEvent = $("<div class='event-add'><a class='event-add-link'><span class='ui-icon ui-icon-plusthick'></span> Add event<br></a></div>");
-					$("iframe#output").contents().find("ul.toc").hover(function() {
+					iframe.find("ul.toc").hover(function(e) {
+						//var $target = $(e.currentTarget);
+						var set = $(this).attr("class").match(/set\-\w{1,}/)[0];
 						$(this).after($addEvent);
+						$addEvent.click(function() {
+							dialog.dialog("open");
+							$("#event-add-form").removeClass();
+							$("#event-add-form").addClass(set); // Let the dialog know which set to put the event in through classes
+						});
 					}, function() {
 						$sortIcon.remove();
 					});
-					
-					$("iframe#output").contents().find("a.event-add-link").click(function() {
-						console.log("test");
-					});
 				});
 			},
-			function(xhr) {
+			function(xhr) { // Failure
 				var data = JSON.parse(xhr.responseText);
 				$('#data').show();
 				$('iframe#output').html('Request failed with code '+ xhr.status +': ' + JSON.stringify(data));
@@ -267,16 +254,23 @@ $(function() {
 		iframe.contentDocument.execCommand('copy');
 		alert("DCD copied to clipboard!\n\nIf that didn't work, use Ctrl+C or Command+C after pressing this button!")
 	});
-	function numberEvents() {
-		var count = 1;
-		$("iframe#output").contents().find("ul.count, div.count").each(function() {
-			if ($(this).hasClass("count-reset")) count = 1;
-			$(this).find("li.toc-event, span.info-event-title").each(function() {
-				var i = 0;
-				$(this).find("span.counter").remove();
-				$(this).prepend("<span class='counter'>"+ count + ".&nbsp;</span>"); // first time numbering
-				count++;
-			});
-		});
-	}
+	
+	dialog = $("#event-add-dialog").dialog({
+		modal: true,
+		height: 600,
+		width: 700,
+		autoOpen: false,
+		buttons: {
+			Create: function() {
+				var set = $("#event-add-form").attr("class");
+				if (set.length > 3) {
+					addEvent(set, getNewEventId(), $("#event-add-name").val(), $("#event-add-info").val(), $("#event-add-notes").val());
+					dialog.dialog("close");
+				} // Otherwise something probably went wrong
+			},
+			Cancel: function() {
+				dialog.dialog("close");
+			}
+		}
+	});
 });
