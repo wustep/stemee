05fbabb69bbbb419b04ab8dc995364d5078a5851
Stephen Wu <wujstep@gmail.com>
2017-08-01 18:59:47 -0400

    deletin needless files

--- resources/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-	// This file is not used at the moment
-	const DB_HOST = 'host';
-	const DB_USER = 'user';
-	const DB_PASS = 'pass';
-	const DB_NAME = 'name';
-	const API_KEY = '5668a0a6945a3199ababf3e17fe4cea817fb6d44520379d6a9515b5eb24d74ce';
-?>
