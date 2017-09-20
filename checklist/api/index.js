86d53976d44d3a63fd1c9b5eca1b75864e5d4608
Stephen Wu <wujstep@gmail.com>
2017-09-20 17:18:23 -0400

    real routing fixes for static files on production

--- checklist/api/index.js
+++ checklist/api/index.js
@@ -15,9 +15,11 @@ app.use('/api', routes);
 if (process.env.NODE_ENV == 'production') {
 	console.log('Server: Serving static assets in production');
 	console.log(`Server: Client assets on port ${port}`);
-	app.use('/', express.static('././build'));
+	app.use(express.static(path.join(__dirname + "/../", 'build')));
+	app.get('/*', (req, res) => {
+		res.sendFile(path.join(__dirname + "/../", 'build', 'index.html'));
+	});
 }
-
 app.listen(port, () => {
 	console.log(`Server: Started on port ${port}`)
 });
